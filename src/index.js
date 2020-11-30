bf565a45aa0b46d1ba5f2d2369b810930ef99201
kailashmaden <nedamkailash12@gmail.com>
2020-11-30 15:09:36 +0545

    header and footer setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './Resources/css/app.css';
+import Routes from './routes';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { BrowserRouter } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from './Hoc/Layout'
+
+const Routes = (props) => {
+  return (
+    <Layout>
+      second lines
+    </Layout>
+  )
+}
+export default Routes;
